b257bfc1d304049a33cd3ba11fc301b308b4e29c
Sevigny <mikael.sevigny@ssc-spc.gc.ca>
2023-04-04 09:30:39 -0400

    Simplify logic of range() function

--- utils.js
+++ utils.js
@@ -2,16 +2,14 @@
 export function range(number1, number2, step = 1) {
   const output = []
 
-  if (number2 === undefined) {
+  if (number2) {
+    for (let i = number1; i < number2; i += step) {
+      output.push(i);
+    }
+  } else {
     for (let i = 0; i < number1; i++) {
       output.push(i);
     }
-
-    return output;
-  }
-
-  for (let i = number1; i < number2; i += step) {
-    output.push(i);
   }
 
   return output;
