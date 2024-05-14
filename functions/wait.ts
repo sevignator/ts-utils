ee69a62e7b3dd03f4b44df85b0d3868d911855a8
Mikaël Sévigny <sevignator@hotmail.com>
2024-05-14 19:29:08 -0400

    Rename parameter

--- functions/wait.ts
+++ functions/wait.ts
@@ -1,8 +1,8 @@
 /**
  * @function Promise-based function to create an arbitrary delay in your program.
  */
-export function wait(delay: number) {
+export function wait(milliseconds: number) {
   return new Promise<void>((resolve) => {
-    setTimeout(resolve, delay);
+    setTimeout(resolve, milliseconds);
   });
 }
