b744ad3fb201737dbfd870521d12c8140b6e41e3
Mikaël Sévigny <sevignator@hotmail.com>
2024-05-16 14:53:19 -0400

    Add FetchWrapper class

---
+++ classes/FetchWrapper.ts
@@ -0,0 +1,40 @@
+type DefaultBody = Record<string, unknown>;
+type MethodTypes = 'POST' | 'PUT' | 'DELETE';
+
+/**
+ * @classdesc Class for making type-safe fetch requests.
+ * Implementation written by Jad Joubran (https://github.com/JadJoubranBV).
+ */
+class FetchWrapper {
+  #baseURL: string;
+
+  constructor(baseURL: string) {
+    this.#baseURL = baseURL;
+  }
+
+  get<T>(endpoint: string): Promise<T> {
+    return fetch(this.#baseURL + endpoint).then((response) => response.json());
+  }
+
+  post<T, B = DefaultBody>(endpoint: string, body: B): Promise<T> {
+    return this.#send<B>('POST', endpoint, body);
+  }
+
+  put<T, B = DefaultBody>(endpoint: string, body: B): Promise<T> {
+    return this.#send<B>('PUT', endpoint, body);
+  }
+
+  delete<T, B = DefaultBody>(endpoint: string, body?: B): Promise<T> {
+    return this.#send<B>('DELETE', endpoint, body);
+  }
+
+  #send<B>(method: MethodTypes, endpoint: string, body?: B) {
+    return fetch(this.#baseURL + endpoint, {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    }).then((response) => response.json());
+  }
+}
