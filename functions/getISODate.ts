78e079c3690223c36d0bf091fb4b70ed40ce0af9
Mikaël Sévigny <sevignator@hotmail.com>
2024-09-30 20:00:53 -0400

    Add getISODate() function

---
+++ functions/getISODate.ts
@@ -0,0 +1,8 @@
+/**
+ * @function Function that accepts a `Date` object and returns only the
+ * date using the ISO format (YYYY-MM-DD).
+ */
+export default function getISODate(dateObj: Date) {
+  const [date] = dateObj.toISOString().split('T');
+  return date;
+}
